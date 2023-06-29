5f1efb8853a94bebd9f91f98f5ced535420cfdd2
osayenko0315 <126353984+osayenko0315@users.noreply.github.com>
2023-06-28 19:10:06 -0500

    index work

--- index.js
+++ index.js
@@ -29,6 +29,34 @@ const questions = [
         massage: 'select shape color'
     },
 ]
-
+function init() {
+    inquirer
+    .promp(questions)
+    .then((data) => {
+        let shapeInput
+        switch (shape) {
+            case "circle":
+                shapeInput = new Circle(shape_color)
+                break;
+            case "triangle":
+                shapeInput = new Triangle(shape_color)
+                break;
+            case "square":
+                    shapeInput = new Square(shape_color)
+                    break;
+        }
+        svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+        fs.writeFile('./examples/logo.svg', svgLogo, err => {
+            if (err) {
+                console.error('error occurred while creating SVG file')
+            } else {
+                console.log('generated logo.svg')
+            }
+        })
+        console.log('Generated logo.svg')
+    })
+    .catch(error => {
+        console.log('error has occurred:', error)
+    })
 }
-init()
+    init()
